776566167aecdad6b24782f84b663372da43a0f0
Jon Rowe <hello@jonrowe.co.uk>
2013-05-23 15:08:54 +1000

    handle no twitter accounts

--- lib/twittermotion/twitter.rb
+++ lib/twittermotion/twitter.rb
@@ -9,9 +9,14 @@ module Twitter
   end
 
   def accounts
-    @accounts ||= self.account_store.accountsWithAccountType(account_type).collect do |ac_account|
-      Twitter::User.new(ac_account)
-    end
+    @accounts ||=
+      begin
+        if self.account_store.accountsWithAccountType(account_type)
+          self.account_store.accountsWithAccountType(account_type).collect do |ac_account|
+            Twitter::User.new(ac_account)
+          end
+        end
+      end
   end
 
   def sign_in(&block)
